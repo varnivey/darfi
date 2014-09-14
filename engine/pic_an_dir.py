73223e9bd04647ff7ae19307ef54b19df5c12e54
satary <satary@yandex.ru>
2014-09-15 00:23:10 +0400

    simple_gui fixed and functional, added calc_foci_in_dirlist to pic_an_dir.py for convinience

--- engine/pic_an_dir.py
+++ engine/pic_an_dir.py
@@ -35,15 +35,28 @@ def calc_multiple_dirs(dir_path, nuclei_name='3DAPI.TIF', foci_name='3FITС.TIF'
 
         print 'Calculation has FINISHED in', os.path.split(subdir)[0]
 
-
 def calc_foci_in_dir(dir_path, nuclei_name='3DAPI.TIF', foci_name='3FITС.TIF', outfile = 'result.txt',\
         sensitivity = 8., min_cell_size = 1500, peak_min_val_perc = 60, foci_min_val_perc = 90,\
         foci_radius = 10, foci_min_level_on_bg = 40, foci_rescale_min = None, foci_rescale_max = None,\
         nuclei_color = 0.66, foci_color = 0.33):
-    '''Calculates foci from dir'''
-
+    '''Calculates foci from dir list'''
+    
     dirs_with_images = [os.path.join(dir_path, directory) for directory in os.listdir(dir_path)]
-
+    
+    calc_foci_in_dirlist(dir_path, dirs_with_images, nuclei_name, foci_name, outfile,\
+        sensitivity, min_cell_size, peak_min_val_perc, foci_min_val_perc,\
+        foci_radius, foci_min_level_on_bg, foci_rescale_min, foci_rescale_max,\
+        nuclei_color, foci_color)
+    
+#THE lasiest way to do the job =D
+def calc_foci_in_dirlist(dir_path, dir_list, nuclei_name='3DAPI.TIF', foci_name='3FITС.TIF', outfile = 'result.txt',\
+        sensitivity = 8., min_cell_size = 1500, peak_min_val_perc = 60, foci_min_val_perc = 90,\
+        foci_radius = 10, foci_min_level_on_bg = 40, foci_rescale_min = None, foci_rescale_max = None,\
+        nuclei_color = 0.66, foci_color = 0.33):
+    '''Calculates foci from dir'''
+    
+    dirs_with_images = dir_list
+    
     pre_image_dirs = [image_dir for image_dir in dirs_with_images if \
             (os.path.isfile(os.path.join(image_dir,nuclei_name)) and os.path.isfile(os.path.join(image_dir, foci_name)))]
 
--- simple_gui/simple_gui.py
+++ simple_gui/simple_gui.py
@@ -1,39 +1,126 @@
+# -*- coding: utf-8 -*-
 import sys,os,functools
 sys.path.append('../engine')
 import pic_an_dir as Engine
 from PyQt4 import QtGui, QtCore
 
+        
+class CheckableDirModel(QtGui.QDirModel):
+    def __init__(self, parent=None):
+        QtGui.QDirModel.__init__(self, None)
+        self.checks = {}
 
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role != QtCore.Qt.CheckStateRole:
+            return QtGui.QDirModel.data(self, index, role)
+        else:
+            if index.column() == 0:
+                return self.checkState(index)
+
+    def flags(self, index):
+        return QtGui.QDirModel.flags(self, index) | QtCore.Qt.ItemIsUserCheckable
+
+    def checkState(self, index):
+        if index in self.checks:
+            return self.checks[index]
+        else:
+            return QtCore.Qt.Unchecked
+#TODO FIXME I'M STUPID =D            
+    def checkAll(self):
+        print self.rowCount()
+        for index in self.checks:
+            self.setData(index,QtCore.QVariant(2),QtCore.Qt.CheckStateRole)
+            
+            
+    def unCheckAll(self):
+        for index in self.checks:
+            self.setData(index,QtCore.QVariant(0),QtCore.Qt.CheckStateRole)
+            
+    def setData(self, index, value, role):
+        if (role == QtCore.Qt.CheckStateRole and index.column() == 0):
+            self.layoutAboutToBeChanged.emit()
+            self.checks[index] = value
+            #self.emit(QtCore.SIGNAL("dataChanged(QModelIndex,QModelIndex)"), index, index)
+            self.layoutChanged.emit()
+            return True 
+
+        return QtGui.QDirModel.setData(self, index, value, role)
 
 class DarfiUI(QtGui.QWidget):
 
     
     def __init__(self):
         super(DarfiUI, self).__init__()
+        self.workDir=QtCore.QDir.homePath()
 
         self.initUI()
 
+    def selectWorkDir(self):
+        self.model.unCheckAll()
+        self.workDir=QtGui.QFileDialog.getExistingDirectory()
+        #print self.workDir
+        self.fileMenu.setRootIndex(self.model.index(self.workDir))
+        #self.model.checkAll()
+
         
+    def updateImages(self):
+        index = self.fileMenu.selectedIndexes()[0]
+        #print QtGui.QDirModel.rowCount(index)
+        path =  self.model.filePath(index)
+        #QtCore.QStringList(filters)
+        filters = ["*.TIF", "*.tif"]
+        imageDir = QtCore.QDir(path)
+        imageDir.setNameFilters(filters)
+        try:
+            imageName1 = imageDir.entryList()[0]
+            imageName2 = imageDir.entryList()[1]
+            pix1 = QtGui.QPixmap(path + "/" + imageName1)
+            self.lbl1.setPixmap(pix1.scaledToWidth(300))
+            self.lbl1.update()
+            pix2 = QtGui.QPixmap(path + "/" + imageName2)
+            self.lbl2.setPixmap(pix2.scaledToWidth(300))
+            self.lbl2.update()
+        except IndexError:
+            print "No data image"
+        
+        #print imageDir.entryList()[0]
+        #print imageList[1].absolutePath()
+
         
     def initUI(self):      
         windowInitWidth = 1024
         windowInitHeight = 768
         hbox = QtGui.QHBoxLayout(self)
         
-        self.model = QtGui.QFileSystemModel()
-        self.model.removeColumn(0)
-        self.model.setRootPath(QtCore.QDir.homePath())
-        self.fileMenu = QtGui.QTreeView(self)
-        self.fileMenu.setHeaderHidden(1)
-        self.fileMenu.setModel(self.model)
-        self.fileMenu.hideColumn(1)
-        self.fileMenu.hideColumn(2)
-        self.fileMenu.hideColumn(3)
-        self.fileMenu.setRootIndex(self.model.index(QtCore.QDir.homePath()))
         
- 
-        imagePreviewArea = QtGui.QFrame(self)
-        imagePreviewArea.setFrameShape(QtGui.QFrame.StyledPanel)
+        fileMenuArea = QtGui.QWidget(self)
+        self.model = CheckableDirModel()
+        self.model.setFilter(QtCore.QDir.Dirs|QtCore.QDir.NoDotAndDotDot)
+        self.fileMenu = QtGui.QListView(self)
+        self.fileMenu.setModel(self.model)
+        self.fileMenu.clicked.connect(lambda: self.updateImages())
+
+        fileMenuLayout = QtGui.QVBoxLayout(fileMenuArea)
+        selectFolderButton = QtGui.QPushButton("Select workdir")
+        selectFolderButton.clicked.connect(lambda: self.selectWorkDir())
+        self.checkAllBox = QtGui.QCheckBox('Check/Uncheck All', self)
+        self.checkAllBox.stateChanged.connect(lambda: (self.model.checkAll() if self.checkAllBox.isChecked() else self.model.unCheckAll()))
+        fileMenuLayout.addWidget(selectFolderButton)
+        fileMenuLayout.addWidget(self.checkAllBox)
+        fileMenuLayout.addWidget(self.fileMenu)
+        
+
+        imagePreviewArea = QtGui.QWidget(self)
+        imagePreviewLayout = QtGui.QHBoxLayout(imagePreviewArea)
+        imagePreviewLayout.setAlignment(QtCore.Qt.AlignTop)
+        #imagePreviewArea.setFrameShape(QtGui.QFrame.StyledPanel)
+
+
+        self.lbl1 = QtGui.QLabel(self)
+        self.lbl2 = QtGui.QLabel(self)
+
+        imagePreviewLayout.addWidget(self.lbl1)
+        imagePreviewLayout.addWidget(self.lbl2)
 
         buttonArea = QtGui.QWidget(self)
         buttonLayout = QtGui.QVBoxLayout(buttonArea)
@@ -42,9 +129,11 @@ class DarfiUI(QtGui.QWidget):
         
         
         runCalcButton = QtGui.QPushButton("Calculate")
-        runCalcButton.clicked.connect(lambda: self.dummyFunction())
+        runCalcButton.clicked.connect(lambda: self.runCalc())
+        
         openSettingsButton = QtGui.QPushButton("Open settings")
         #buttonLayout.addWidget(loadButton)
+        loadButton.clicked.connect(lambda: self.runCalc())
         buttonLayout.addWidget(runCalcButton)
         #buttonLayout.addWidget(openSettingsButton)
         buttonLayout.setAlignment(QtCore.Qt.AlignTop)
@@ -64,27 +153,29 @@ class DarfiUI(QtGui.QWidget):
         splitter2.setSizes([windowInitHeight*3/4,windowInitHeight/4])
         
         splitter3 = QtGui.QSplitter(QtCore.Qt.Horizontal)
-        splitter3.addWidget(self.fileMenu)
+        splitter3.addWidget(fileMenuArea)
         splitter3.addWidget(splitter2)
         splitter3.setSizes([windowInitWidth/4,windowInitWidth*3/4])
 
         hbox.addWidget(splitter3)
         self.setLayout(hbox)
-        QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
+        #QtGui.QApplication.setStyle(QtGui.QStyleFactory.create('Cleanlooks'))
         
         self.setGeometry(0, 0,windowInitWidth, windowInitHeight)
         self.setWindowTitle('DARFI')
         self.show()
 
-    def dummyFunction(self):
-        #fileInfo = self.model.fileInfo()
-        try:
-            indexItem = self.fileMenu.selectedIndexes()[0]
-            fileName = self.model.fileName(indexItem)
-            filePath = self.model.filePath(indexItem)
-            Engine.calc_foci_in_dir(str(filePath))
-        except IndexError:
-            print "select correct folder"
+    def runCalc(self):
+        print "hello"
+        dirsWithImages = []
+        for index,value in self.model.checks.items():
+            if value.toBool():
+                dirsWithImages.append(str(self.model.filePath(index)))
+        #print self.workDir,dirsWithImages
+        Engine.calc_foci_in_dirlist(str(self.workDir),dirsWithImages)
+                 
+          
+
         
 def main():
     
